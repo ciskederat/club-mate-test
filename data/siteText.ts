d863e2b7bfd76cadaabe7e4bf82550876291f8a9
Cis verhaert <cis@MacBook-Air-van-Cis-2.local>
2026-05-17 19:42:54 +0200

    update website

---
+++ data/siteText.ts
@@ -0,0 +1,7 @@
+export const introPopupText = {
+  firstParagraph:
+    "Mate Alert toont plekken waar er Club Mate is gespot. Klik op een pin voor adres, openingsuren en voorraadmeldingen.",
+  secondParagraph:
+    "Zie je ergens Club Mate staan? Gebruik de grote Mate Alert knop onderaan om die spot toe te voegen. Klopt een locatie niet meer, meld dan dat Club Mate niet meer aanwezig is.",
+  buttonLabel: "Starten",
+};
